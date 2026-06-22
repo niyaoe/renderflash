0e82ad6882492903b23ded79351bfc1fb04a6124
niyaoe <niya4334@gmail.com>
2026-06-22 13:18:49 +0530

    reddit ui layout

--- src/Pages/Feed_Section/Layout/RenderFlashLayout.jsx
+++ src/Pages/Feed_Section/Layout/RenderFlashLayout.jsx
@@ -1,57 +1,109 @@
 import { NavLink, Outlet } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
-import { MdOutlineDynamicFeed } from "react-icons/md";
-import { RiGlobalFill } from "react-icons/ri";
-import { CgProfile } from "react-icons/cg";
+import {
+  FaSearch,
+  FaHome,
+  FaUsers,
+} from "react-icons/fa";
+import {
+  MdOutlineDynamicFeed,
+} from "react-icons/md";
+import {
+  RiGlobalFill,
+} from "react-icons/ri";
+import {
+  CgProfile,
+} from "react-icons/cg";
+
+import { IoPeople } from "react-icons/io5";
+
 import "./RenderFlashLayout.css";
 
 export default function RenderFlashLayout() {
   return (
-    <div className="rf-layout-wrapper">
-      {/* ===== HEADER ===== */}
-      <header className="rf-main-header">
-        <div className="rf-header-inner rf-container">
-          {/* ⭐ LOGO ROW */}
-          <div className="rf-header-top">
-            <div className="rf-feed-logo">
-              renderFlash<span style={{ color: "white" }}>.io</span>
-            </div>
+    <div className="reddit-layout">
 
-            <div className="rf-header-icons">
-              <NavLink to="users">
-                <i class="bi bi-people-fill"></i>
-              </NavLink>
-              <NavLink to="notifications">
-                <i className="bi bi-bell-fill notification-icon"></i>
-              </NavLink>
-              <NavLink to="upload">
+      {/* HEADER */}
+      <header className="reddit-header">
+        <div className="reddit-header-inner">
+
+          <div className="rf-feed-logo">
+            renderFlash<span>.io</span>
+          </div>
+
+          <div className="reddit-search">
+            <FaSearch />
+            <input placeholder="Search RenderFlash" />
+          </div>
+
+          <div className="reddit-actions">
+            <NavLink to="users">
+              <IoPeople />
+            </NavLink>
+
+            <NavLink to="notifications">
+              <i className="bi bi-bell-fill"></i>
+            </NavLink>
+
+            <NavLink to="upload">
               <i className="bi bi-plus-square"></i>
-              </NavLink>
-            </div>
+            </NavLink>
           </div>
 
-          {/* TABS */}
-          <div className="rf-feed-tabs">
-            <NavLink to="feed" className="rf-tab-btn">
-              <MdOutlineDynamicFeed />
-            </NavLink>
-            <NavLink to="search" className="rf-tab-btn">
-              <FaSearch />
-            </NavLink>
-            <NavLink to="chat" className="rf-tab-btn">
-              <RiGlobalFill />
-            </NavLink>
-            <NavLink to="profile" className="rf-tab-btn">
-              <CgProfile />
-            </NavLink>
-          </div>
         </div>
       </header>
 
-      {/* ===== PAGE CONTENT ===== */}
-      <main className="rf-layout-content">
-        <Outlet />
-      </main>
+      {/* BODY */}
+      <div className="reddit-body">
+
+        {/* SIDEBAR */}
+        <aside className="reddit-sidebar">
+          <NavLink to="feed">
+            <MdOutlineDynamicFeed />
+            Feed
+          </NavLink>
+
+          <NavLink to="search">
+            <FaSearch />
+            Search
+          </NavLink>
+
+          <NavLink to="chat">
+            <RiGlobalFill />
+            Explore
+          </NavLink>
+
+          <NavLink to="profile">
+            <CgProfile />
+            Profile
+          </NavLink>
+        </aside>
+
+        {/* CONTENT */}
+        <main className="reddit-content">
+          <Outlet />
+        </main>
+
+      </div>
+
+      {/* MOBILE BOTTOM NAV */}
+      <nav className="reddit-mobile-nav">
+        <NavLink to="feed">
+          <MdOutlineDynamicFeed />
+        </NavLink>
+
+        <NavLink to="search">
+          <FaSearch />
+        </NavLink>
+
+        <NavLink to="chat">
+          <RiGlobalFill />
+        </NavLink>
+
+        <NavLink to="profile">
+          <CgProfile />
+        </NavLink>
+      </nav>
+
     </div>
   );
 }
